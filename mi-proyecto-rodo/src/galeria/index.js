483b1b8b78c1db5a70823c0aa951e4d86da29741
luisrodolfoduarte <duarteluis2014@hotmail.com>
2020-01-27 17:52:15 -0300

    subo una parte de los ejercicios sin terminar

---
+++ mi-proyecto-rodo/src/galeria/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import "./style.css"
+
+function Galeria(){
+    return(
+        <div className="galeriaP">
+            <div className="titulo">
+                <h1>titulo de la galeria</h1>
+            </div>
+            <div className="cartasP">
+                <div className="cartas">
+
+                </div>
+                <div className="cartas">
+
+                </div>
+                <div className="cartas">
+
+                </div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Galeria;
